788a72c978aa2dc0c5ab963b09e5b91f95c95238
Julian Wagner <julian.wagner@mni.thm.de>
2017-02-05 23:35:52 +0100

    create Paint for field lines

--- app/src/main/java/com/example/standard/tictactoe/TicTacToeView.java
+++ app/src/main/java/com/example/standard/tictactoe/TicTacToeView.java
@@ -1,12 +1,21 @@
 package com.example.standard.tictactoe;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.View;
 
 public class TicTacToeView extends View {
+    private Paint linePaint;
+
     public TicTacToeView(Context context) {
         super(context);
 
         setBackgroundResource(R.drawable.background);
+
+        linePaint = new Paint();
+        linePaint.setColor(Color.RED);
+        linePaint.setStrokeWidth(5);
+        linePaint.setStyle(Paint.Style.STROKE);
     }
 }
