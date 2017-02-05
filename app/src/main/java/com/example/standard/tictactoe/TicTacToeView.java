a44383d93bc215edf775fdb2ea096217562aff98
Julian Wagner <julian.wagner@mni.thm.de>
2017-02-05 23:47:14 +0100

    draw field

--- app/src/main/java/com/example/standard/tictactoe/TicTacToeView.java
+++ app/src/main/java/com/example/standard/tictactoe/TicTacToeView.java
@@ -1,6 +1,7 @@
 package com.example.standard.tictactoe;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.View;
@@ -17,5 +18,18 @@ public class TicTacToeView extends View {
         linePaint.setColor(Color.RED);
         linePaint.setStrokeWidth(5);
         linePaint.setStyle(Paint.Style.STROKE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        final int width = canvas.getWidth() / 3;
+        final int height = canvas.getHeight() / 3;
+
+        for (int i = 1; i <= 2; i++) {
+            canvas.drawLine(0, height * i, getWidth(), height * i, linePaint);
+            canvas.drawLine(width * i, 0, width * i, getHeight(), linePaint);
+        }
     }
 }
