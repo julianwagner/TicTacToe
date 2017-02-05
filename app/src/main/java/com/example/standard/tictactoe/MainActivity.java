7c1b15287a65cd5b750253d770516fbb4cdb6fe5
Julian Wagner <julian.wagner@mni.thm.de>
2017-02-05 22:57:08 +0100

    add TicTacToe field to user interface

--- app/src/main/java/com/example/standard/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/standard/tictactoe/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.standard.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private TicTacToeView ticTacToeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // add TicTacToe field to UI
+        ticTacToeView = new TicTacToeView(this);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.linearLayout0);
+        layout.addView(ticTacToeView);
     }
 }
