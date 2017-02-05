f38eeb46c8b6041f11cc768c57f264e8fac64f67
Julian Wagner <julian.wagner@mni.thm.de>
2017-02-05 23:08:51 +0100

    create View TicTacToeView

---
+++ app/src/main/java/com/example/standard/tictactoe/TicTacToeView.java
@@ -0,0 +1,10 @@
+package com.example.standard.tictactoe;
+
+import android.content.Context;
+import android.view.View;
+
+public class TicTacToeView extends View {
+    public TicTacToeView(Context context) {
+        super(context);
+    }
+}
