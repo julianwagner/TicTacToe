a98a92f413e1078dce1a8f4e9809883b8e37496f
Julian Wagner <julian.wagner@mni.thm.de>
2017-02-07 12:20:06 +0100

    create global var board

--- app/src/main/java/com/example/standard/tictactoe/Board.java
+++ app/src/main/java/com/example/standard/tictactoe/Board.java
@@ -2,8 +2,10 @@ package com.example.standard.tictactoe;
 
 public class Board {
     private static Board instance;
+    private int[][] board;
 
     private Board() {
+        board = new int[3][3];
     }
 
     public static Board getInstance() {
