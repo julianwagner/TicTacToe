f5f60403770110063e7cd732fcb19826bc54858c
Julian Wagner <julian.wagner@mni.thm.de>
2017-02-07 16:54:17 +0100

    add calculateMove

--- app/src/main/java/com/example/standard/tictactoe/MoveGenerator.java
+++ app/src/main/java/com/example/standard/tictactoe/MoveGenerator.java
@@ -2,6 +2,8 @@ package com.example.standard.tictactoe;
 
 public class MoveGenerator {
     private static MoveGenerator instance;
+    private int depth, bestMove;
+    private int[] board;
 
     private MoveGenerator() {
     }
@@ -11,5 +13,13 @@ public class MoveGenerator {
             MoveGenerator.instance = new MoveGenerator();
         }
         return MoveGenerator.instance;
+    }
+
+    public int calculateMove(int[] board, int depth) {
+        this.depth = depth;
+        bestMove = -1;
+        this.board = board;
+        max(-1, depth);
+        return bestMove;
     }
 }
