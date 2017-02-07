c3eb1ea9089f4187b2da0dd6e55a42a10c329ce2
Julian Wagner <julian.wagner@mni.thm.de>
2017-02-07 19:28:08 +0100

    reworked MoveGenerator

--- app/src/main/java/com/example/standard/tictactoe/MoveGenerator.java
+++ app/src/main/java/com/example/standard/tictactoe/MoveGenerator.java
@@ -1,44 +1,44 @@
 package com.example.standard.tictactoe;
 
-public class MoveGenerator {
+class MoveGenerator {
     private static MoveGenerator instance;
-    private int depth, bestMove;
+    private int bestMove;
     private Board board;
 
     private MoveGenerator() {
     }
 
-    public static MoveGenerator getInstance() {
+    static MoveGenerator getInstance() {
         if (MoveGenerator.instance == null) {
             MoveGenerator.instance = new MoveGenerator();
         }
         return MoveGenerator.instance;
     }
 
-    public int calculateMove(Board board, int depth) {
-        this.depth = depth;
+    int calculateMove() {
         bestMove = -1;
-        this.board = board;
-        max(-1, depth);
+        board = Board.getInstance();
+        max(-1, 4);
         return bestMove;
     }
 
     private int max(int player, int depth) {
-        if (board.gameOver() == player * -1) {
-            return -1;
-        } else if (board.gameOver() == player) {
-            return 1;
+        int gameOver = board.gameOver();
+        if (gameOver == player * -1) {
+            return -100;
+        } else if (gameOver == player) {
+            return 100;
         } else if (depth == 0) {
             return 0;
         }
         int maxValue = Integer.MIN_VALUE;
         for (int i = 0; i < 9; i++) {
-            board.makeMove(i, player);
-            int value = min(player * -1, depth - 1);
-            board.undoMove();
-            if (value > maxValue) {
-                maxValue = value;
-                if (depth == 0) {
+            if (board.getStatus(i) == 0) {
+                board.makeMove(i);
+                int value = min(player * -1, depth - 1);
+                board.undoMove();
+                if (value > maxValue) {
+                    maxValue = value;
                     bestMove = i;
                 }
             }
@@ -47,20 +47,23 @@ public class MoveGenerator {
     }
 
     private int min(int player, int depth) {
-        if (board.gameOver() == player * -1) {
-            return 1;
-        } else if (board.gameOver() == player) {
-            return -1;
+        int gameOver = board.gameOver();
+        if (gameOver == player * -1) {
+            return 100;
+        } else if (gameOver == player) {
+            return -100;
         } else if (depth == 0) {
             return 0;
         }
         int minValue = Integer.MAX_VALUE;
         for (int i = 0; i < 9; i++) {
-            board.makeMove(i, player);
-            int value = min(player * -1, depth - 1);
-            board.undoMove();
-            if (value < minValue) {
-                minValue = value;
+            if (board.getStatus(i) == 0) {
+                board.makeMove(i);
+                int value = max(player * -1, depth - 1);
+                board.undoMove();
+                if (value < minValue) {
+                    minValue = value;
+                }
             }
         }
         return minValue;
