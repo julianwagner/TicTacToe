a12e153155e5ff0206067077ba44871c9a07fae3
Julian Wagner <julian.wagner@mni.thm.de>
2017-02-07 12:37:05 +0100

    create makeMoveAndroid

--- app/src/main/java/com/example/standard/tictactoe/Board.java
+++ app/src/main/java/com/example/standard/tictactoe/Board.java
@@ -19,6 +19,19 @@ public class Board {
         return false;
     }
 
+    public void makeMoveAndroid() {
+        if (nextPlayer == -1) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board.length; j++) {
+                    if (board[i][j] == 0) {
+                        board[i][j] = -1;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
     public static Board getInstance() {
         if (Board.instance == null) {
             Board.instance = new Board();
