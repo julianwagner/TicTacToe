32040e4360035d65e0365bf98c6600de131f742e
Julian Wagner <julian.wagner@mni.thm.de>
2017-02-07 17:10:12 +0100

    create max

--- app/src/main/java/com/example/standard/tictactoe/MoveGenerator.java
+++ app/src/main/java/com/example/standard/tictactoe/MoveGenerator.java
@@ -3,7 +3,7 @@ package com.example.standard.tictactoe;
 public class MoveGenerator {
     private static MoveGenerator instance;
     private int depth, bestMove;
-    private int[] board;
+    private Board board;
 
     private MoveGenerator() {
     }
@@ -15,11 +15,34 @@ public class MoveGenerator {
         return MoveGenerator.instance;
     }
 
-    public int calculateMove(int[] board, int depth) {
+    public int calculateMove(Board board, int depth) {
         this.depth = depth;
         bestMove = -1;
         this.board = board;
         max(-1, depth);
         return bestMove;
+    }
+
+    private int max(int player, int depth) {
+        if (board.gameOver() == player * -1) {
+            return -1;
+        } else if (board.gameOver() == player) {
+            return 1;
+        } else if (depth == 0) {
+            return 0;
+        }
+        int maxValue = Integer.MIN_VALUE;
+        for (int i = 0; i < 9; i++) {
+            board.makeMove(i, player);
+            int value = min(player * -1, depth - 1);
+            board.undoMove();
+            if (value > maxValue) {
+                maxValue = value;
+                if (depth == 0) {
+                    bestMove = i;
+                }
+            }
+        }
+        return maxValue;
     }
 }
