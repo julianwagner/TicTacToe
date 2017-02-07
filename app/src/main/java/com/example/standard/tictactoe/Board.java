bf49d49cf819fc65a9ee0d2e26f389e8b169fda8
Julian Wagner <julian.wagner@mni.thm.de>
2017-02-07 15:48:31 +0100

    rework makeMoveAndroid

--- app/src/main/java/com/example/standard/tictactoe/Board.java
+++ app/src/main/java/com/example/standard/tictactoe/Board.java
@@ -22,12 +22,10 @@ public class Board {
     public boolean makeMoveAndroid() {
         if (nextPlayer == -1) {
             for (int i = 0; i < board.length; i++) {
-                for (int j = 0; j < board.length; j++) {
-                    if (board[i][j] == 0) {
-                        board[i][j] = -1;
-                        nextPlayer = 1;
-                        return true;
-                    }
+                if (board[i] == 0) {
+                    board[i] = -1;
+                    nextPlayer = 1;
+                    return true;
                 }
             }
         }
